2e764e7c31ebe61c7ce8df81fbc138ab67698348
Lucas <lukaaks1@gmail.com>
2025-10-28 17:22:08 -0300

    mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    reporter: "mochawesome",
+    "reporterOptions": {
+      "reportDir": "mochawesome-report",
+      "overwrite": false,
+      "reportFilename": "index.html",
+      "html": true,
+      "json": false
+    },
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
